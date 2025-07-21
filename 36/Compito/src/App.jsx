9f21deb593745106995182b945bb14f58540fc90
Gabriele Fontana <gafreax@gmail.com>
2025-07-22 00:15:37 +0200

    lezione 36: primo compito

--- 36/Compito/src/App.jsx
+++ 36/Compito/src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 const statoIniziale = [
   { color: 'red', count: 0 },
   { color: 'blue', count: 0 },
-  { color: 'green', count: 0 },
+  { color: 'green', count: 0 }
 ]
 
-function App() {
+function App () {
   const [listaBottoni, setListaBottoni] = useState(statoIniziale)
   const [logMessage, setLogMessage] = useState('')
 
@@ -20,28 +20,32 @@ function App() {
     )
   }
 
-  useEffect( () => {
+  useEffect(() => {
     console.log('useEffect chiamato per aggiornare il contatore di colore')
     setLogMessage(
       `Contatori aggiornati: ${listaBottoni.map(bottone => `${bottone.color}: ${bottone.count}`).join(', ')}`
     )
-  }, [listaBottoni] )
+  }, [listaBottoni])
 
-  return <div className="container">
-    <h1>Compito 36: useState avanzao</h1>
-    <p>Elenco Bottone</p>
-    <div>
-      {
-        listaBottoni.map( (bottone, index) =>
-          <button key={index} className="btn p-2 m-3 fw-bold"
+  return (
+    <div className='container'>
+      <h1>Compito 36: useState avanzao</h1>
+      <p>Elenco Bottone</p>
+      <div>
+        {
+        listaBottoni.map((bottone, index) =>
+          <button
+            key={index} className='btn p-2 m-3 fw-bold'
             style={{ backgroundColor: bottone.color, color: 'white' }}
-            onClick={() => aggiornaContatore(bottone.color)}>
-              Numero click {bottone.count}
-          </button> )
+            onClick={() => aggiornaContatore(bottone.color)}
+          >
+            Numero click {bottone.count}
+          </button>)
       }
+      </div>
+      {logMessage && <pre>{logMessage}</pre>}
     </div>
-    {logMessage && <pre>{logMessage}</pre>}
-  </div>
+  )
 }
 
 export default App
