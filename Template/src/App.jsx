c59b4fccd2d6d3adbd09a5ab8d917f828108e1ae
Gabriele Fontana <gafreax@gmail.com>
2025-07-22 00:16:42 +0200

    fix formatting

--- Template/src/App.jsx
+++ Template/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 
-function App() {
-
-  return <>
-    <h1>React + Bootstrap Template</h1>
-    <p>Inserire qui il proprio codice</p>
-  </>
+function App () {
+  return (
+    <>
+      <h1>React + Bootstrap Template</h1>
+      <p>Inserire qui il proprio codice</p>
+    </>
+  )
 }
 
 export default App
