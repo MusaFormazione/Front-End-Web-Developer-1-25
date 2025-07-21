1a2eb0becab81e3fd4ca12bf9263a0082bc461d9
Gabriele Fontana <gafreax@gmail.com>
2025-07-21 02:33:13 +0200

    up compito 36

--- 36/Compito2/src/App.jsx
+++ 36/Compito2/src/App.jsx
@@ -2,11 +2,27 @@ import { useState, useEffect, useRef } from 'react'
 
 import './App.css'
 
-function Nota ({nota, rimuovi}) {
-  return <li key={nota.id}>
-    { nota.testo }
-    <button className='m-2 btn btn-small btn-secondary' onClick={() => rimuovi(nota.id)}>rimuovi</button>
+function Nota({ nota, rimuovi }) {
+  const btnClassName = 'm-2 btn btn-outline-danger btn-sm'
+  return <li key={nota.id} className='list-group-item'>
+    {nota.testo}
+    <button className={btnClassName} onClick={() => rimuovi(nota.id)}>
+      rimuovi <i class="bi bi-trash ml-1"></i>
+    </button>
   </li>
+}
+
+function AggiungiNota({ handleAggiungi, nota }) {
+  return <div className='d-flex justify-content-between align-items-center'>
+      <div>
+        <input className='form-control' ref={nota} placeholder='inserisci una nuova nota' />
+      </div>
+      <div>
+        <button className='btn btn-primary' onClick={handleAggiungi}>
+          aggiungi <i class="bi bi-plus-circle"></i>
+        </button>
+      </div>
+  </div>
 }
 
 function App() {
@@ -14,17 +30,17 @@ function App() {
   const nota = useRef(null)
 
   const rimuovi = (id) => {
-    setNote( prev => prev.filter( nota => nota.id !== id ) )
+    setNote(prev => prev.filter(nota => nota.id !== id))
   }
 
   const aggiungi = (testo) => {
-    const id = note.length ? note[note.length-1].id + 1 : 0// Notare come calcoliamo il nuovo nuovoId
+    const id = note.length ? note[note.length - 1].id + 1 : 0// Notare come calcoliamo il nuovo nuovoId
     console.log(note)
-    setNote( prev => ([...prev, { id, testo }]))  // Notare l'utilizzo diretto di ID e TESTO
+    setNote(prev => ([...prev, { id, testo }]))  // Notare l'utilizzo diretto di ID e TESTO
   }
 
   const handleAggiungi = () => {
-    if ( nota.current ) {
+    if (nota.current) {
       const testo = nota.current?.value
       aggiungi(testo)
     }
@@ -32,17 +48,16 @@ function App() {
     nota.current.focus() // notare il focus
   }
 
-  useEffect( () => console.log('note modificate') , [note] )
+  useEffect(() => console.log('note modificate'), [note])
 
   return <div className='container'>
     <h1>Compito 36: Lista Note</h1>
-    <ul>
-      { note && note.map( nota => <Nota key={nota.id} nota={nota} rimuovi={rimuovi} /> ) }
+    <ul className='list-group col-6'>
+      {note && note.map(nota => <Nota key={nota.id} nota={nota} rimuovi={rimuovi} />)}
+      <li className='list-group-item'>
+        <AggiungiNota handleAggiungi={handleAggiungi} nota={nota} />
+      </li>
     </ul>
-    <div className='d-flex g-2'>
-      <input className='form-control' ref={nota} placeholder='inserisci una nuova nota'/>
-      <button className='m-2 btn btn-primary' onClick={handleAggiungi}>aggiungi</button>
-    </div>
   </div>
 }
 
