ec597da3299e078526fa2ce462f8fb7e83b889cb
Gabriele Fontana <gafreax@gmail.com>
2025-08-04 00:05:58 +0200

    add scss

---
+++ 39/ReactSass/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App () {
+  return <div className="container">
+      <h1>React Sass</h1>
+      <p>Inserire qui il proprio codice</p>
+      <div className='esempi'>
+        <div className='esempio'>Esempio spacer x 2</div>
+        <div className='esempio-2'>Esempio spacer x 4</div>
+      </div>
+    </div>
+}
+
+export default App
